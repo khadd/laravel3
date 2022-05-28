e6d4b6fa7341074bf33bc949b67fc2adb45b6fef
khadd <kh.abdualaziz@gmail.com>
2022-05-28 12:19:36 +0300

    add procfile & composer.lock

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,6 +45,12 @@
                     </svg>
                 </div>
 
+                <div>
+                    <h1>
+                        مرحباً انا اسمي خديجة عبدالعزيز
+                    </h1>
+                </div>
+
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
