12d36ca27f5d58e6df61ef93eea72c885909f3ff
khadd <kh.abdualaziz@gmail.com>
2022-06-03 21:12:01 +0300

    add me and remove the word myname

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,7 +46,7 @@
                 </div>
                 
                  <div>
-                    <h1 style="text-align:center;">مرحباً اسمي خديجة عبدالعزيز </h1>
+                    <h1 style="text-align:center;">مرحباً أنا خديجة عبدالعزيز </h1>
                 </div>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
