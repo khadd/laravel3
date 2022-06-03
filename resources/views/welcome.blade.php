1ea2535913617baea09a55a5a38b41f8f02fc489
khadd <kh.abdualaziz@gmail.com>
2022-06-03 18:50:28 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,6 +44,10 @@
                         </g>
                     </svg>
                 </div>
+                
+                 <div>
+                    <h1 style="text-align:center;">مرحباً اسمي خديجة عبدالعزيز </h1>
+                </div>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
